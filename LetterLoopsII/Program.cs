148435ca58bca06c184a07df0c14ee0f1711062f
Nathan Pabst <nathan.pabst@gmail.com>
2018-08-26 10:29:40 -0500

    finished exercise and added gitignore

---
+++ LetterLoopsII/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LetterLoopsII
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //INPUT = RqaEzty OUTPUT = R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy
+        {
+            var input = "RqaEzty";
+            Console.WriteLine($"The original input was: {input}");
+            //string.Join() method concatenates the results with a dash between each element
+            //The Select extension method with 'c' and 'i' parameters returns an uppercase version of the character followed by a string of lowercase characters in the length of the index.
+            var modifiedInput = string.Join("-", input.Select((c, i) => Char.ToUpper(c) + new string(Char.ToLower(c), i)));
+            Console.WriteLine($"The modified input is: {modifiedInput}");
+
+            Console.WriteLine("press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
